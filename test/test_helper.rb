869d16c95b71a7311ff52c2f9a136a28cd1841a3
Michal Olejniczak <olejniczvk@gmail.com>
2018-11-11 19:51:31 +0100

    Set window size to Selenium driver

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,7 +29,7 @@ class ActiveSupport::TestCase
 
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu] }
+      chromeOptions: { args: %w[headless disable-gpu window-size=1920,1080] }
     )
 
     Capybara::Selenium::Driver.new app,
