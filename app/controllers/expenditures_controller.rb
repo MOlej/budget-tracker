2b1d3b4e23a176881b2e439a7df055352ab45345
Michal Olejniczak <olejniczvk@gmail.com>
2018-11-13 12:28:24 +0100

    Move setting default date for expenditures to the model

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -6,7 +6,6 @@ class ExpendituresController < ApplicationController
 
   def create
     @expenditure = Expenditure.new(user_params)
-    set_defaults_for_missing_params
     @expenditure.save
 
     redirect_to expenditures_path
@@ -22,12 +21,5 @@ class ExpendituresController < ApplicationController
 
     def user_params
       params.require(:expenditure).permit(:amount, :title, :category, :date)
-    end
-
-    def set_defaults_for_missing_params
-      @expenditure.amount = 0 unless params.values[2][:amount].present?
-      @expenditure.title = "Untitled" unless params.values[2][:title].present?
-      @expenditure.category = "Uncategorised" unless params.values[2][:category].present?
-      @expenditure.date = Time.zone.today unless params.values[2][:date].present?
     end
 end
--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,8 +1,14 @@
 class Expenditure < ApplicationRecord
-# validates :amount, presence: true
-# validates :title, presence: true, length: { maximum: 50 }
+  before_save :set_defaults
 
   default_scope -> { order(date: :desc) }
+
+  def set_defaults
+      self.amount ||= 0
+      self.title = "Untitled" if self.title.empty?
+      self.category = "Uncategorised" if self.category.empty?
+      self.date ||= Time.zone.today
+  end
 
   CATEGORIES = [
     ['Auto & Transport',
@@ -10,14 +16,14 @@ class Expenditure < ApplicationRecord
     ['Bills',
       ['Electricity', 'Gas', 'Heating', 'Internet', 'Television', 'Phone', 'Rent', 'Water']],
     ['Entertainment',
-     ['Games', 'Events', 'Movies', 'Travel']],
+      ['Games', 'Events', 'Movies', 'Travel']],
     ['Food & Dining',
-     ['Alcohol', 'Coffee shops', 'Fast Food', 'Groceries', 'Restaurant']],
+      ['Alcohol', 'Coffee shops', 'Fast Food', 'Groceries', 'Restaurant']],
     ['Home',
-     ['Accessories and Furnishing', 'Garden', 'Home Insurance', 'Renovation', 'Services']],
+      ['Accessories and Furnishing', 'Garden', 'Home Insurance', 'Renovation', 'Services']],
     ['Kids',
       ['Activies', 'Allowance', 'Baby Supplies', 'Babysitter & Daycare', 'School Supplies', 'Toys']],
     ['Personal',
-     ['Books', 'Clothing', 'Education', 'Electronics & Software', 'Health & Fitness', 'Hobbies', 'Newspapers & Magazines']]
+      ['Books', 'Clothing', 'Education', 'Electronics & Software', 'Health & Fitness', 'Hobbies', 'Newspapers & Magazines']]
   ]
 end
