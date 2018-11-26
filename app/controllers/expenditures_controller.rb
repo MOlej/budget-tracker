4b7656ac169e527e4a3d826be7efe3a0ae602ac6
Michal Olejniczak <olejniczvk@gmail.com>
2018-11-27 00:20:37 +0100

    Fix indentation and remove doubled sorting method

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -2,7 +2,13 @@ class ExpendituresController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @expenditures = Expenditure.search(params[:search]).order(Arel.sql("#{case_insensitive(sort_column)} #{sort_direction}"), date: :desc, created_at: :desc)
+    @expenditures = Expenditure
+      .search(params[:search])
+      .order(
+        Arel.sql("#{case_insensitive(sort_column)} #{sort_direction}"),
+        date: :desc,
+        created_at: :desc
+      )
     @expenditure = Expenditure.new
   end
 
@@ -21,19 +27,19 @@ class ExpendituresController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:expenditure).permit(:amount, :title, :category, :date)
-    end
+  def user_params
+    params.require(:expenditure).permit(:amount, :title, :category, :date)
+  end
 
-    def sort_column
-      Expenditure.column_names.include?(params[:column]) ? params[:column] : 'date'
-    end
+  def sort_column
+    Expenditure.column_names.include?(params[:column]) ? params[:column] : 'date'
+  end
 
-    def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
-    end
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+  end
 
-    def case_insensitive(column)
-      %w[title category].include?(column) ? "lower(#{column})" : column
-    end
+  def case_insensitive(column)
+    %w[title category].include?(column) ? "lower(#{column})" : column
+  end
 end
